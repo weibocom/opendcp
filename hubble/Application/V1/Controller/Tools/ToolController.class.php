62738c3dd834eb5075d84bbfd8f9f897188c5bed
yabo <yabo@staff.weibo.com>
2017-06-19 14:52:04 +0800

    修改初始化SQL

--- hubble/Application/V1/Controller/Tools/ToolController.class.php
+++ hubble/Application/V1/Controller/Tools/ToolController.class.php
@@ -140,8 +140,12 @@ class ToolController extends RestController{
         if($ret['code'] != 1)
             $this->ajaxReturn(std_error(' get init Alteration_Type '.$ret['msg']));
 
+        $content = json_decode($ret['content'],true);
+        $content['group_id'] = $gid;
+        $content['script_id'] = $uid;
+
         $data = $ret['content'];
-        $ret = $AlterationType->add($data['type'],$data['name'],$data['content'],'system', $bidArg);
+        $ret = $AlterationType->add($data['type'],$data['name'],json_encode($content),'system', $bidArg);
 
         if($ret['code'] == 1)
             $this->ajaxReturn(std_error($ret['msg']));
@@ -177,6 +181,32 @@ class ToolController extends RestController{
         if($ret['code'] != HUBBLE_RET_SUCCESS)
             $this->ajaxReturn(std_error(' set init shell_upstream_conf '.$ret['msg']));
 
+        $sid = $ret['content'];
+        
+        //服务注册
+        $AlterationType = new AlterationType();
+
+        $ret = $AlterationType->exist(['biz_id' => $bidArg]);
+        if($ret['code'] == 1)
+            $this->ajaxReturn(std_error("get init Alteration_Type of $bidArg exist"));
+
+        if($ret['code'] == 2)
+            $this->ajaxReturn(std_error("get init Alteration_Type error: db error"));
+
+        $ret = $AlterationType->exist(['id' => 1, 'biz_id' => 0]) ;
+        if($ret['code'] != 1)
+            $this->ajaxReturn(std_error(' get init Alteration_Type '.$ret['msg']));
+
+        $content = json_decode($ret['content'],true);
+        $content['group_id'] = $gid;
+        $content['script_id'] = $sid;
+
+        $data = $ret['content'];
+        $ret = $AlterationType->add($data['type'],$data['name'],json_encode($content),'system', $bidArg);
+
+        if($ret['code'] == 1)
+            $this->ajaxReturn(std_error($ret['msg']));
+        
         $this->ajaxReturn(std_return());
     }
 
