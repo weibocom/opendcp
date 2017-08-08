3b2decb20039e3517c62d9ff8a44e58642366da5
wangjianhui <wongjianhui@163.com>
2017-08-08 11:57:00 +0800

    Revise the redirection of the ssh cmd stdout

--- deploy/manage_device/manage_device.sh
+++ deploy/manage_device/manage_device.sh
@@ -20,7 +20,7 @@ checkImage(){
 echo "---Begin to execute the init operation in this instance---"
 echo "###################begin:"`date +%Y-%m-%d" "%H":"%M":"%S`
 #打印参数
-#echo "param:"$*
+echo "param:"$* >>result.out
 if [ -z "$1" ]; then
     echo "mysql address is  empty! you should be set like this:"
     echo "sh get.sh mysql://DBUSER:@DBADDRESS/DBNAME?charset=utf8"
@@ -56,30 +56,47 @@ fi
 
 #1、安装docker
 echo "1、安装docker"
-yum install -y docker >/dev/null 2>&1
+echo "1、安装docker" >>result.out
+yum install -y docker >>result.out
+if [ $? -ne 0 ]; then
+   echo "install docker failed"
+   exit 1
+fi
 
 #2、修改docker配置
 echo "2、修改docker配置"
+echo "2、修改docker配置" >>result.out
 #文件 /etc/sysconfig/docker 追加如下两行
 echo 'OPTIONS="-g=/data0/docker -s=devicemapper --label idc=aliyun"' >> /etc/sysconfig/docker
 echo 'INSECURE_REGISTRY="--insecure-registry docker.io --insecure-registry '$6'"' >> /etc/sysconfig/docker
 
 #3、重新启动docker
 echo "3、重新启动docker"
-service docker restart >/dev/null 2>&1
+echo "3、重新启动docker" >>result.out
+service docker restart >>result.out
+if [ $? -ne 0 ]; then
+    echo "restart docker service failed"
+    exit 1
+fi
 
-echo "4、下载octans-agent镜像"
 #4、下载octans-agent镜像
+echo "4、下载octans-agent镜像"
+echo "4、下载octans-agent镜像" >>result.out
+docker pull registry.cn-beijing.aliyuncs.com/opendcp/octans-agent:2.0 >>result.out
+if [ $? -ne 0 ]; then
+    echo "pull docker image failed"
+    exit 1
+fi
 
-docker pull registry.cn-beijing.aliyuncs.com/opendcp/octans-agent:2.0 >/dev/null 2>&1
-
-echo "5、检查镜像是否下载成功"
 #5、检查镜像是否下载成功
+echo "5、检查镜像是否下载成功"
+echo "5、检查镜像是否下载成功" >>result.out
 checkImage registry.cn-beijing.aliyuncs.com/opendcp/octans-agent  2.0
 
 
 #6、启动octans-agent容器，并且修改配置(通过环境变量设置到容器内部)
 echo "6、启动octans-agent容器，并且修改配置(通过环境变量设置到容器内部)"
+echo "6、启动octans-agent容器，并且修改配置(通过环境变量设置到容器内部)" >>result.out
 
 #hn=`hostname`
 #echo "hostname=" $hn
@@ -94,7 +111,7 @@ echo "6、启动octans-agent容器，并且修改配置(通过环境变量设置
 #10.85.41.168:8083
 #i-2zeen6mal4s9qvpqb4iq
 #47.93.162.228
-docker run -d -e "mysql_url=$1" -e "get_key_url=$2" -e "report_url=$3" -e "instance_id=$4" -e "ssh_port=$7" --net=host --name octans-agent registry.cn-beijing.aliyuncs.com/opendcp/octans-agent:2.0
+docker run -d -e "mysql_url=$1" -e "get_key_url=$2" -e "report_url=$3" -e "instance_id=$4" -e "ssh_port=$7" --net=host --name octans-agent registry.cn-beijing.aliyuncs.com/opendcp/octans-agent:2.0 >>result.out
 
 #检查octans是否启动
 TIMES=5
@@ -110,7 +127,11 @@ do
         sleep 2
 done
 
-docker exec octans-agent python /data/octans/octans/tool/auto_report.py $4
+docker exec octans-agent python /data/octans/octans/tool/auto_report.py $4 >>result.out
+if [ $? -ne 0 ]; then
+    echo "report instance status failed"
+    exit 1
+fi
 
 echo "[DONE] --------------"
 echo "###################end:"`date +%Y-%m-%d" "%H":"%M":"%S`
